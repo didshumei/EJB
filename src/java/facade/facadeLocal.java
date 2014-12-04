052f6a64878d9f34d5db3d80d2ed30a29822f7ab
sylvainbron <sylvainbron@172.16.151.44>
2014-12-04 14:17:30 +0100

    Merge origin/master

--- src/java/facade/facadeLocal.java
+++ src/java/facade/facadeLocal.java
@@ -5,6 +5,7 @@
 package facade;
 
 import java.util.ArrayList;
+import java.util.Date;
 import javax.ejb.Local;
 import metier.Adresse;
 import metier.Article;
@@ -67,4 +68,80 @@ public interface facadeLocal
      * méthodes MoyenPaiement
      */
     String getTypeCB();
+    
+     /*
+        METHODES CATALOGUE
+    */
+   
+    void commencerReduction(Article article, float tauxReduction);
+    void terminerReduction(Article article);
+    void modifierPrixArticle(String referenceArticle, float nouveauPrix);
+    void retirerArticle(String referenceArticle);
+    ArrayList<Article> afficherArticles();
+    void creerCategorie(String libelleCategorie);
+    
+    /*
+        METHODES CATEGORIE
+    */
+    
+    
+    /*
+        METHODES GENRE
+    */
+    
+    void setIdGenre(int idGenre);
+    String getTitreGenre();
+    void setTitreGenre(String titreGenre);
+    String getReferenceGenre();
+    void setReferenceGenre(String referenceGenre);
+    void creerGenre(String libelleGenre);
+    
+    /*
+    * METHODES BORDEREAULIVRAISON
+    */
+    String getContenu();
+    void setContenu(String contenu);
+    void remplirContenu();
+    
+    /*
+    * METHODES REMBOURSEMENT
+    */
+    Date getDate();
+    void setDate(Date date);
+    void genererLettre();
+    void genererCheque();
+    
+    /*
+    * METHODES CHEQUE
+    */
+    float getMontant();
+    void setMontant(float montant);
+    String getNumero();
+    void setNumero(String numero);
+    
+    /*
+    * METHODES LETTRE
+    */
+    String getContenuLettre();
+    void setContenuLettre(String contenu);
+    
+    /*
+    * METHODES ACCUSERECEPTION
+    */
+    Date getDateReception(); 
+    void setDateReception(Date dateReception);
+    
+    /*
+    * METHODES LIGNECOMMANDE
+    */
+    
+    int getQuantiteCommande();
+    void setQuantiteCommande(int quantite);
+    int getQuantiteLivree();
+    void setQuantiteLivree(int quantite);
+    float getSstotal();
+    void setSstotal(float sstotal);
+    boolean isIsARembourser();
+    void setIsARembourser(boolean isARembourser);
 }
+
